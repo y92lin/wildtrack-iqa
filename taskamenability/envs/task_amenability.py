40fa3a0bb3ce99f6e0a027d29e79f20768091ffa
Yolanda Davis <yolanda.m.davis@gmail.com>
2022-11-05 06:55:39 -0400

    Task Amenability and Project Refactor (#3)
    * git ignore
    
    * task amend components
    
    task amend components
    
    * refactor updates
    
    * fixes to resizing issue
    
    * Created using Colaboratory
    
    * fixes to calculation
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * update to use species classification
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * update to reward calculation
    
    * model tuning
    
    * Created using Colaboratory
    
    * add save callback
    
    * increase check freq
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * move multitask files
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * refactor to use just train and test data
    
    * final model tuning changes
    
    * Created using Colaboratory
    
    * update readme and remove unused script

---
+++ taskamenability/envs/task_amenability.py
@@ -0,0 +1,90 @@
+import torch
+import gym
+import numpy as np
+from gym import spaces
+
+from keras import layers
+import keras
+from sklearn.metrics import accuracy_score
+
+
+class TaskAmenability(gym.Env):
+
+    def __init__(self, x_train, y_train, x_val, y_val, x_holdout, y_holdout, task_predictor, img_shape):
+
+        self.x_train, self.y_train = x_train, y_train
+        self.x_val, self.y_val = x_val, y_val
+        self.x_holdout, self.y_holdout = x_holdout, y_holdout
+
+        self.img_shape = img_shape
+        self.task_predictor = task_predictor
+
+        self.controller_batch_size = 32
+        self.task_predictor_batch_size = 4
+        self.epochs_per_batch = 2
+
+        self.img_shape = img_shape
+
+        self.observation_space = spaces.Box(low=0, high=14, shape=self.img_shape, dtype=np.float32)
+        self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
+        self.reward_range =  [-1, +1]
+
+        self.actions_list = []
+        self.val_metric_list = [0.4] * 10
+
+        self.sample_num_count = 0
+
+    def get_batch(self):
+        shuffle_inds = np.random.permutation(len(self.y_train)).astype(int)
+        self.x_train, self.y_train = self.x_train[shuffle_inds], self.y_train[shuffle_inds]
+        return self.x_train[:self.controller_batch_size], self.y_train[:self.controller_batch_size]
+
+    def compute_moving_avg(self):
+        self.val_metric_list = self.val_metric_list[-10:]
+        moving_avg = np.mean(self.val_metric_list)
+        return moving_avg
+
+    def get_val_acc_vec(self):
+        val_acc_vec = []
+        for i in range(len(self.y_train_batch)):
+            with torch.no_grad():
+                obs = self.x_train_batch[i].permute(2, 0, 1).unsqueeze(dim=0).float()
+                y_pred = self.task_predictor(obs)
+                y_pred = y_pred.argmax(dim=1).squeeze().item()
+                val_metric = accuracy_score(self.y_train_batch[i:i + 1], np.array([y_pred]))
+                val_acc_vec.append(val_metric)
+        return np.array(val_acc_vec)
+
+    def step(self, action):
+        self.actions_list.append(action)
+        self.sample_num_count += 1
+
+        if self.sample_num_count <= self.controller_batch_size:
+            reward = 0
+            done = False
+            return self.x_data[self.sample_num_count - 1], reward, done, {}
+        else:
+            moving_avg = self.compute_moving_avg()
+            val_acc_vec = self.get_val_acc_vec()
+            val_sel_vec = self.actions_list[:self.controller_batch_size]
+            val_sel_vec = np.rint(np.array(val_sel_vec).flatten())
+            val_metric = np.mean(np.array([1 if sel == acc else 0 for sel, acc in zip(val_sel_vec, val_acc_vec)]))
+            self.val_metric_list.append(val_metric)
+
+            print("Performance Accuracy")
+            print(val_metric)
+            reward = val_metric - moving_avg
+            done = True
+            print("Reward: " + str(reward))
+            return np.random.rand(self.img_shape[0], self.img_shape[1], self.img_shape[2]), reward, done, {}
+
+    def reset(self):
+        self.x_train_batch, self.y_train_batch = self.get_batch()
+        self.x_data = self.x_train_batch
+        self.y_data = self.y_train_batch
+        self.actions_list = []
+        self.sample_num_count = 0
+        return self.x_train_batch[self.sample_num_count]
+
+    def save_task_predictor(self, task_predictor_save_path):
+        self.task_predictor.save(task_predictor_save_path)
---
+++ taskamenability/interface.py
@@ -0,0 +1,74 @@
+from taskamenability.envs.task_amenability import TaskAmenability
+
+import numpy as np
+import os
+
+from stable_baselines import PPO2
+from stable_baselines.common.vec_env import DummyVecEnv
+from taskamenability.save_callback import SaveOnBestTrainingRewardCallback
+from taskamenability.vec_monitor import VecMonitor
+
+from keras.models import load_model
+
+class PPOInterface():
+    def __init__(self, x_train, y_train, x_val, y_val, x_holdout, y_holdout, task_predictor, img_shape,
+                 load_models=False, controller_save_path=None, task_predictor_save_path=None):
+
+        self.x_holdout, self.y_holdout = x_holdout, y_holdout
+        self.log_dir = "tmp/"
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        if load_models:
+            self.task_predictor = load_model(task_predictor_save_path)
+        else:
+            self.task_predictor = task_predictor
+
+        def make_env():
+            return TaskAmenability(x_train, y_train, x_val, y_val, x_holdout, y_holdout, task_predictor, img_shape)
+
+        self.env = DummyVecEnv([make_env])
+
+        self.env = VecMonitor(self.env, self.log_dir)
+
+        def get_from_env(env, parameter):
+            return env.get_attr(parameter)[0]
+
+        # number of steps per episode (controller_batch_size ) multiply by an integer to do multiple episodes before controller update  # number of steps per episode (controller_batch_size + val_set_len) multiply by an integer to do multiple episodes before controller update
+        self.n_rollout_steps = get_from_env(self.env, 'controller_batch_size')
+
+        if load_models:
+            assert isinstance(controller_save_path, str)
+            self.load(save_path=controller_save_path)
+        else:
+            self.model = PPO2('CnnPolicy',
+                              self.env,
+                              nminibatches=1,
+                              n_steps=self.n_rollout_steps,
+                              gamma=1.0,
+                              verbose=2,
+                              seed=None,
+                              learning_rate=0.00010)
+
+    def train(self, num_episodes):
+
+        callback = SaveOnBestTrainingRewardCallback(check_freq=10, log_dir=self.log_dir)
+        time_steps = int(num_episodes * self.n_rollout_steps)
+        print(f'Training started for {num_episodes} episodes:')
+        self.model.learn(total_timesteps=time_steps, callback=callback)
+
+    def get_controller_preds_on_holdout(self):
+        actions = []
+        for i in range(len(self.x_holdout)):
+            pred = self.model.predict(self.x_holdout[i, :, :, :])[0][0]
+            actions.append(pred)
+
+        return np.array(actions)
+
+    def save(self, controller_save_path, task_predictor_save_path):
+        self.model.save(controller_save_path)
+
+    def load(self, save_path):
+        self.model = PPO2.load(save_path)
+        self.model.set_env(self.env)
+
+    # implement saving and loading task predictor
